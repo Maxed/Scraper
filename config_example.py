e93c070359eef0b575af3000f2e34c5a3d18be62
maxed <austinwestvik@gmail.com>
2017-07-29 23:15:48 -0700

    added example config file

---
+++ config_example.py
@@ -0,0 +1,10 @@
+# add your own id/secret/subreddits
+
+reddit_id = "id"
+reddit_secret = "secret"
+user_agent = "idk wat this does"
+
+imgur_id = "id"
+imgur_secret = "secret"
+
+subreddit = "pics"
